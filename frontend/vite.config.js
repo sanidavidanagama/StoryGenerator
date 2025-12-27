db4eb6035f87d715f9c9d5b37b03db5acff7d42e
sanidavidanagama <sanidavidanagama@gmail.com>
2025-12-27 22:56:16 +0530

    feat(proxy): add server proxy to integrate with the backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,4 +10,13 @@ export default defineConfig({
       },
     }),
   ],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 })
