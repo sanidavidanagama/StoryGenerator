676aaa6bcd3b315fdb86e13119e48680827ea2c1
sanidavidanagama <sanidavidanagama@gmail.com>
2025-12-28 00:15:35 +0530

    feat: setting up project requirements for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,28 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-        secure: false
-      }
+export default defineConfig((command, mode) => {
+  const env = loadEnv(mode, process.cwd(), "")
+
+  console.log(env.VITE_DEBUG)
+  return {
+    plugins: [react({
+        babel: {
+          plugins: [['babel-plugin-react-compiler']],
+        },
+      }),],
+    server: {
+      ...(env.VITE_DEBUG === "true" && {
+          proxy: {
+            "/api": {
+              target: "http://localhost:8000",
+              changeOrigin: true,
+              secure: false
+            }
+          }
+      })
     }
   }
-})
+}
+)
